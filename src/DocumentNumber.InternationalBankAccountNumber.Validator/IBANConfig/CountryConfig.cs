66f3c13dd34a6429189444896243f0ab424e9deb
Marco Enxuto <marco@enxuto.pt>
2026-06-01 16:12:13 +0100

    Added support for IBAN Country validation. Added a config file and POCO class to manage it.

---
+++ src/DocumentNumber.InternationalBankAccountNumber.Validator/IBANConfig/CountryConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+namespace DocumentNumber.InternationalBankAccountNumber.Validator.IBANConfig
+{
+  internal sealed class CountryConfig
+  {
+    [JsonPropertyName("code")]
+    internal string Code { get; set; }
+    [JsonPropertyName("length")]
+    internal int Length { get; set; }
+  }
+}
